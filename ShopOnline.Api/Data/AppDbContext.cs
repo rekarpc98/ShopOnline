eb97c1150d177cfcba18bba0bad44b1c74d4f7ed
rekardev1 <rekardev1@gmail.com>
2023-11-18 18:37:18 +0300

    Add DbSet's

--- ShopOnline.Api/Data/AppDbContext.cs
+++ ShopOnline.Api/Data/AppDbContext.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Models.Entities;
 
 namespace ShopOnline.Api.Data;
 
 public class AppDbContext(IConfiguration configuration) : DbContext
 {
+	public DbSet<User> Users { get; set; } = default!;
+	public DbSet<Product> Products { get; set; } = default!;
+	public DbSet<Card> Cards { get; set; } = default!;
+	public DbSet<ProductCategory> ProductCategories { get; set; } = default!;
+	public DbSet<CardItem> CardItems { get; set; } = default!;
+
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
 		string? connectionString = configuration.GetConnectionString("DefaultConnection");
