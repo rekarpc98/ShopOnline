fed6fe8e54f2a96b8d2ba488666adb13ee0989e2
rekarpc98 <rekarpc98@gmail.com>
2023-12-09 18:06:57 +0300

    Add Get Product By Id & Post Product Functionality

--- ShopOnline.Web/Repositories/Implementations/ProductRepository.cs
+++ ShopOnline.Web/Repositories/Implementations/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Json;
 using ShopOnline.Models.Dtos;
+using ShopOnline.Models.Requests.Products;
 using ShopOnline.Web.Constants;
 using ShopOnline.Web.Mappers;
 using ShopOnline.Web.Models.Entities.Products;
@@ -9,6 +10,25 @@ namespace ShopOnline.Web.Repositories.Implementations;
 
 public class ProductRepository(HttpClient httpClient) : IProductRepository
 {
+	public async ValueTask<Product> PostProductAsync(Product product)
+	{
+		var postProduct = new PostProduct
+		{
+			Name = product.Name,
+			Description = product.Description,
+			ImageUrl = product.ImageUrl,
+			Price = product.Price,
+			QuantityInStock = product.QuantityInStock,
+			CategoryId = product.CategoryId
+		};
+
+		HttpResponseMessage response = await httpClient.PostAsJsonAsync(ApiRoutes.Products, postProduct);
+		// todo: handle exceptions
+		var productDtos = await response.Content.ReadFromJsonAsync<ProductDto>();
+		
+		return productDtos!.FromDto();
+	}
+	
 	public async ValueTask<List<Product>> GetProductsAsync()
 	{
 		var productDtos = await httpClient.GetFromJsonAsync<List<ProductDto>>(ApiRoutes.Products);
@@ -17,9 +37,24 @@ public class ProductRepository(HttpClient httpClient) : IProductRepository
 		{
 			return [];
 		}
-		
+
 		IEnumerable<Product> products = productDtos.FromDtos();
-		
+
+		return products.ToList();
+	}
+
+	public async ValueTask<List<Product>> GetProductByIdAsync(Guid id)
+	{
+		var path = $"{ApiRoutes.Products}/{id}";
+		var productDtos = await httpClient.GetFromJsonAsync<List<ProductDto>>(path);
+
+		if (productDtos is null || productDtos.Count == 0)
+		{
+			return [];
+		}
+
+		IEnumerable<Product> products = productDtos.FromDtos();
+
 		return products.ToList();
 	}
 }
--- ShopOnline.Web/Repositories/Interfaces/IProductRepository.cs
+++ ShopOnline.Web/Repositories/Interfaces/IProductRepository.cs
@@ -4,5 +4,7 @@ namespace ShopOnline.Web.Repositories.Interfaces;
 
 public interface IProductRepository
 {
+	ValueTask<Product> PostProductAsync(Product product);
 	ValueTask<List<Product>> GetProductsAsync();
+	ValueTask<List<Product>> GetProductByIdAsync(Guid id);
 }
