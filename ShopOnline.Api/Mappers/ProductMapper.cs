eac984eeeb92e18ca034274b950ece312db031c6
rekarpc98 <rekarpc98@gmail.com>
2023-12-09 15:34:56 +0300

    Add Product Mapper

---
+++ ShopOnline.Api/Mappers/ProductMapper.cs
@@ -0,0 +1,12 @@
+﻿using Riok.Mapperly.Abstractions;
+using ShopOnline.Api.Models.Dtos;
+using ShopOnline.Api.Models.Entities.Products;
+
+namespace ShopOnline.Api.Mappers;
+
+[Mapper]
+public static partial class ProductMapper
+{
+	public static partial ProductDto ToProductDto(this Product product);
+	public static partial IEnumerable<ProductDto> ToProductsDto(this IEnumerable<Product> products);
+}
